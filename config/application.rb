eb1d7690170831bc71215905ce506c675e0feee7
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-05-17 20:37:33 +0900

    change default controller remove helper, test and javascript(coffee) file to generate new controller then rails g controller messages

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
