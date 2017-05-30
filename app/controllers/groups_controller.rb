5256ac034c1aa55ca8d13a0aa539bb5075fe5eac
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-05-31 00:51:17 +0900

    Define edit and update action on groups controller.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,22 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @users = User.all
+    @group = Group.find(params[:id])
+    @group_users = @group.users
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path. notice: "グループ更新しました"
+    else
+      flash.now[:alert] = "グループ更新に失敗しました"
+      render action: :edit
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name,user_ids:[])
