756b005f8388843c1b810328144e521de755a82a
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-05-31 00:02:39 +0900

    Define group routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
+  resources :groups, except: [:show, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
