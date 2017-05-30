428f30401bb422eadf05760e9e43b4d023a8f9fc
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-05-30 23:39:29 +0900

    Add column to user_groups table. Reflect user_groups table on DB

--- db/migrate/20170530143645_create_user_groups.rb
+++ db/migrate/20170530143645_create_user_groups.rb
@@ -1,7 +1,8 @@
 class CreateUserGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :user_groups do |t|
-
+      t.references :user, foreign_key: true
+      t.references :group, foreign_key: true
       t.timestamps
     end
   end
