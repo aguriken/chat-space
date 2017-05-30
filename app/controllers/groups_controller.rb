0fed8999127a11b5487671a8e6d729b9e35ffc4f
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-05-31 00:10:16 +0900

    Define new and create action.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,22 @@
 class GroupsController < ApplicationController
+
+  def nwe
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      flash.now[:alert] = "グループ名を入力してください"
+      render action: :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name,user_ids:[])
+  end
+
 end
