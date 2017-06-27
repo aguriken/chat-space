2de351e674f3562fac7d7a75fd66175df886517e
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-06-27 16:20:48 +0900

    Edit messages.js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,34 +1,46 @@
 $(function() {
   function buildHTML(message) {
 
-    var html = `<p class="chat-messages__message__header--name" id="li">
-                  ${ message.name }
-                </p>
-                <p class="chat-messages__message__header--time" id="li">
-                  ${ message.date }
-                </p>
-                <p class="chat-messages__message__content" id="li">
-                  ${ message.body }
-                </p>`;
+    var html = `<li class="chat-messages__message">
+                  <div class="chat-messages__message__header">
+                    <p class="chat-messages__message__header--name">
+                      ${ message.name }
+                    </p>
+                    <p class="chat-messages__message__header--time" id="li">
+                      ${ message.date }
+                    </p>
+                  </div>
+                  <p class="chat-messages__message__content" id="li">
+                    ${ message.body }
+                  </p>
+                </li>`;
     return html;
   }
-  
+
   $('.js-form').on('submit', function(e) {
     e.preventDefault();
-    var formDate = new FormDate(this);
-    var href = window.location.href + '/messages'
+    console.log(this)
+    var formData = new FormData(this);
+    console.log( formData );
+    var href = window.location.href
+    console.log (href)
     $.ajax({
       url: href,
       type: "POST",
-      date: formDate,
-      dateType: 'json',
-      processDate: false,
+      data: formData,
+      dataType: 'json',
+      processData: false,
       contentType: false
     })
-    .done(function(date){
-      var html = buildHTML(date);
-      $('').append(html)
+    .done(function(data){
+      console.log("seiko");
+      var html = buildHTML(data);
+      console.log(html);
+      $('.chat-messages-list').append(html)
       $('.chat-footer__body--text').val('')
+    })
+    .fail(function(){
+      alert('error');
     })
   })
 });
