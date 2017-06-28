6a8d97eb728cf08c3d1a3a84f3a9cea2f62fde57
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-06-28 10:23:49 +0900

    Edit rotes.rb to create users search function

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :groups, except: [:show, :destroy] do
-    resources :messages,only: [:index, :create]
+    resources :messages, only: [:index, :create]
+  end
+
+  resources :users, only: [] do
+    collection do
+      get 'search'
+    end
   end
 end
