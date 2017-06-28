59a3c4cb764428d9d415fe89028b489e2bf7834b
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-06-28 10:36:18 +0900

    Edit users_controller to search users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def search
+    @user = User.where("id != #{current_user.id} and name LIKE(?)", "%#{params[:keyword]}%")
+    respond to do |format|
+      format.html
+      format.json
+    end
+  end
+
 end
