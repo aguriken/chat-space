f4e8f44493d4ee7ef2f70149ee5bfcf15c2baecd
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-06-30 12:16:37 +0900

    Edit routes (add index action to groups routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :groups, except: [:show, :destroy] do
+  resources :groups, except: [:index, :show, :destroy] do
     resources :messages, only: [:index, :create]
   end
 
@@ -11,3 +11,4 @@ Rails.application.routes.draw do
     end
   end
 end
+
