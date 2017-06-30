eb82f800caf8ef5cb119250e250e7dba5adbbb7f
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-06-30 12:14:44 +0900

    Modify users_controllers.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,8 @@
 class UsersController < ApplicationController
-
   def search
-    @user = User.where("id != #{current_user.id} and name LIKE(?)", "%#{params[:keyword]}%")
-    respond to do |format|
-      format.html
-      format.json
+    @users = User.where("id != #{ current_user.id } and name LIKE(?)", "%#{ params[:keyword] }%")
+    respond_to do |format|
+        format.json
     end
   end
-
 end
