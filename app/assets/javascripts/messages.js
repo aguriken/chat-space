f307e7b05f0ac95c6d0c55037fca3d56bd133048
中原賢一 <ohagurokenichi@yahoo.co.jp>
2017-07-15 11:50:17 +0900

    Edit messages.js to get image data via json format.

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,16 +1,24 @@
 $(document).on('turbolinks:load', function() {
   function buildHTML(message) {
+    if (message.image){
+      var image = `< ${ message.image } >`
+    } else {
+      var image = ''
+    };
 
     var html = `<li class="chat-messages__message">
                   <div class="chat-messages__message__header">
                     <p class="chat-messages__message__header--name">
                       ${ message.name }
                     </p>
-                    <p class="chat-messages__message__header--time" id="li">
+                    <p class="chat-messages__message__header--time">
                       ${ message.date }
                     </p>
                   </div>
-                  <p class="chat-messages__message__content" id="li">
+                  <p class="chat-messages__message__header--picture">
+                    <img src="${ message.image }">
+                  </p>
+                  <p class="chat-messages__message__content">
                     ${ message.body }
                   </p>
                 </li>`;
@@ -20,7 +28,10 @@ $(document).on('turbolinks:load', function() {
   $('.js-form').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
+    // console.log(this)
+    // console.log(formData)
     var href = window.location.href
+    // console.log(href)
     $.ajax({
       url: href,
       type: "POST",
@@ -30,12 +41,16 @@ $(document).on('turbolinks:load', function() {
       contentType: false
     })
     .done(function(data){
+      // console.log(data);
       var html = buildHTML(data);
+      // console.log(html);
       $('.chat-messages-list').append(html)
-      $('.chat-footer__body--text').val('')
+      $('.js-form')[0].reset();
+      $('input').prop('disabled', false);
     })
     .fail(function(data){
       alert("メッセージを入力してください。");
+      $('input').prop('disabled', false);
     })
   })
 });
